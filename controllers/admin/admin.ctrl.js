b63e00ed68663eea7b325178d869dc1db02c402d
protect-me <pmfwiww@gmail.com>
2021-05-31 13:40:56 +0900

    Feat: show menu

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -23,7 +23,13 @@ exports.post_shops_write = async (req, res) => {
 
 exports.get_shops_detail = async (req, res) => {
   try {
-    const shop = await models.Shops.findByPk(req.params.id);
+    // const shop = await models.Shops.findByPk(req.params.id);
+    const shop = await models.Shops.findeOne({
+      where: {
+        id: req.params.id,
+      },
+      includes: ["Menu"],
+    });
     res.render("admin/detail.html", { shop });
   } catch (e) {
     console.log(e);
