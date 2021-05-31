dcca51a2e1f834e423c8dab41badebbf0ca910bf
protect-me <pmfwiww@gmail.com>
2021-05-31 13:17:54 +0900

    Feat: add menu

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,96 +1,69 @@
-const models = require('../../models');
+const models = require("../../models");
 
-exports.get_shops = async ( _ , res ) => {
+exports.get_shops = async (_, res) => {
+  try {
+    const shops = await models.Shops.findAll();
 
-    try{
-
-        const shops = await models.Shops.findAll();
-
-        res.render( 'admin/shops.html' , { shops });
-
-    }catch(e){
-
-    }
-
-}
-
-exports.get_shops_write = ( _ , res ) => {
-  res.render( 'admin/form.html' );
-}
-
-exports.post_shops_write = async (req,res) => {
-
-    try{
-
-				await models.Shops.create(req.body);
-        res.redirect('/admin/shops');
-
-    }catch(e){
-        console.log(e);
-    }
+    res.render("admin/shops.html", { shops });
+  } catch (e) {}
 };
 
-exports.get_shops_detail = async(req, res) => {
+exports.get_shops_write = (_, res) => {
+  res.render("admin/form.html");
+};
 
-    try{
+exports.post_shops_write = async (req, res) => {
+  try {
+    await models.Shops.create(req.body);
+    res.redirect("/admin/shops");
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-			const shop = await models.Shops.findByPk(req.params.id);
-      res.render('admin/detail.html', { shop });  
+exports.get_shops_detail = async (req, res) => {
+  try {
+    const shop = await models.Shops.findByPk(req.params.id);
+    res.render("admin/detail.html", { shop });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-    }catch(e){
-        console.log(e)
-    }
+exports.get_shops_edit = async (req, res) => {
+  try {
+    const shop = await models.Shops.findByPk(req.params.id);
+    res.render("admin/form.html", { shop });
+  } catch (e) {}
+};
 
+exports.post_shops_edit = async (req, res) => {
+  try {
+    await models.Shops.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.redirect("/admin/shops/detail/" + req.params.id);
+  } catch (e) {}
+};
 
-}
+exports.get_shops_delete = async (req, res) => {
+  try {
+    await models.Shops.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.redirect("/admin/shops");
+  } catch (e) {}
+};
 
-
-exports.get_shops_edit = async(req, res) => {
-
-    try{
-
-        const shop = await models.Shops.findByPk(req.params.id);
-        res.render('admin/form.html', { shop });  
-
-    }catch(e){
-
-    }
-
-
-}
-
-exports.post_shops_edit = async(req, res) => {
-
-    try{
-
-        await models.Shops.update(
-            req.body , 
-            { 
-                where : { id: req.params.id } 
-            }
-        );
-        res.redirect('/admin/shops/detail/' + req.params.id );
-
-    }catch(e){
-
-    }
-
-}
-
-exports.get_shops_delete = async(req, res) => {
-
-    try{
-
-        await models.Shops.destroy({
-            where: {
-                id: req.params.id
-            }
-        });
-        res.redirect('/admin/shops');
-
-    }catch(e){
-
-    }
-
-}
-
+exports.add_menu = async (req, res) => {
+  try {
+    const shop = await models.Shops.findByPk(req.params.id);
+    // create + as에 적은 내용 ( shops.js association 에서 적은 내용 )
+    await shop.createMenu(req.body);
+    res.redirect("/admin/shops/detail/" + req.params.id);
+  } catch (e) {
+    console.log(e);
+  }
+};
--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,21 +1,24 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const ctrl = require('./admin.ctrl');
+const ctrl = require("./admin.ctrl");
 
-router.get('/shops', ctrl.get_shops );
+router.get("/shops", ctrl.get_shops);
 
-router.get('/shops/write', ctrl.get_shops_write );
+router.get("/shops/write", ctrl.get_shops_write);
 
-router.post('/shops/write', ctrl.post_shops_write );
+router.post("/shops/write", ctrl.post_shops_write);
 
-router.get('/shops/detail/:id', ctrl.get_shops_detail );
+router.get("/shops/detail/:id", ctrl.get_shops_detail);
 
-router.get('/shops/edit/:id', ctrl.get_shops_edit );
+router.get("/shops/edit/:id", ctrl.get_shops_edit);
 
-router.post('/shops/edit/:id', ctrl.post_shops_edit );
+router.post("/shops/edit/:id", ctrl.post_shops_edit);
 
-router.get('/shops/delete/:id', ctrl.get_shops_delete );
+router.get("/shops/delete/:id", ctrl.get_shops_delete);
 
-
+// 메뉴 작성
+router.post("/shops/detail/:id", ctrl.add_menu);
+// 메뉴 삭제
+// router.get("/shops/delete/:shop_id/:menu_id", ctrl.remove_menu);
 
 module.exports = router;
