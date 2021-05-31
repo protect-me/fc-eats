4ef1bfb75d606b341f14dc800165afba222f6255
protect-me <pmfwiww@gmail.com>
2021-05-31 12:46:46 +0900

    Feat: Create models(ShopsMenu)

--- models/Shops.js
+++ models/Shops.js
@@ -1,21 +1,26 @@
-const moment = require('moment');
+const moment = require("moment");
 
 module.exports = (sequelize, DataTypes) => {
-    const Shops = sequelize.define('Shops',
-        {
-            id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-            name : { type: DataTypes.STRING , comment: '상점명' },
-            address : { type: DataTypes.STRING , comment: '주소' },
-            location : { type: DataTypes.STRING , comment: '상세주소' },
-            phone : { type: DataTypes.STRING , comment: '전화번호' },
-            open_time : { type: DataTypes.STRING , comment: '운영시간' },
-            cell_phone : { type: DataTypes.STRING , comment: '핸드폰번호' }
-        }
-    );
+  const Shops = sequelize.define("Shops", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, comment: "상점명" },
+    address: { type: DataTypes.STRING, comment: "주소" },
+    location: { type: DataTypes.STRING, comment: "상세주소" },
+    phone: { type: DataTypes.STRING, comment: "전화번호" },
+    open_time: { type: DataTypes.STRING, comment: "운영시간" },
+    cell_phone: { type: DataTypes.STRING, comment: "핸드폰번호" },
+  });
 
-    Shops.prototype.dateFormat = (date) => (
-        moment(date).format('YYYY-MM-DD')
-    );
+  Shops.associate = (models) => {
+    Shops.hasMany(models.ShopsMenu, {
+      as: "Menu",
+      foreignKey: "shop_id",
+      sourceKey: "id",
+      onDelete: "CASCADE",
+    });
+  };
 
-    return Shops;
-}
+  Shops.prototype.dateFormat = (date) => moment(date).format("YYYY-MM-DD");
+
+  return Shops;
+};
