bfa820a9dd4fabc49bdba3c02edb60aa6a31f917
protect-me <pmfwiww@gmail.com>
2021-06-11 12:34:37 +0900

    Feat: chatting between users

--- helpers/socketConnection.js
+++ helpers/socketConnection.js
@@ -1,8 +1,22 @@
+require("./removeByValue")();
+
 module.exports = (io) => {
+  let userList = [];
+
   io.on("connection", (socket) => {
     //아래 두줄로 passport의 req.user의 데이터에 접근한다.
     const session = socket.request.session.passport;
     const user = typeof session !== "undefined" ? session.user : "";
+
+    if (!userList.includes(user.displayname)) {
+      userList.push(user.displayname);
+    }
+    io.emit("join", userList);
+
+    socket.on("disconnect", () => {
+      userList.removeByValue(user.displayname);
+      io.emit("leave", userList);
+    }); // disconnect - 예약어
 
     //사용자 명과 메시지를 같이 반환한다.
     socket.on("client message", (data) => {
