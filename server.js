f049457131768fef7810b9da8047c00f39a91db9
protect-me <pmfwiww@gmail.com>
2021-06-11 10:33:14 +0900

    Feat: construct chatting

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const app = require("./app.js");
 const port = 3000;
 
-app.listen(port, function () {
+const server = app.listen(port, function () {
   console.log("Express listening on port", port);
+});
+const listen = require("socket.io");
+const io = listen(server);
+
+io.on("connection", (socket) => {
+  console.log("소켓서버접속");
+  socket.on("client message", (data) => {
+    io.emit("server message", data.message);
+  });
 });
