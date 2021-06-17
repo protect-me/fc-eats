65971d5f13e3518c3873ebc6a59df42dfb7f7111
protect-me <pmfwiww@gmail.com>
2021-06-18 00:58:36 +0900

    Feat: limit cart

--- controllers/shops/shops.ctrl.js
+++ controllers/shops/shops.ctrl.js
@@ -8,10 +8,17 @@ exports.get_shops_detail = async (req, res) => {
 
   let cartList = {}; //장바구니 리스트
   let cartLength = 0;
+  let sameShops = true;
+
   if (typeof req.cookies.cartList !== "undefined") {
     cartList = JSON.parse(unescape(req.cookies.cartList));
     cartLength = Object.keys(cartList).length;
+    for (let key in cartList) {
+      if (parseInt(cartList[key].shop_id) !== parseInt(req.params.id)) {
+        sameShops = false;
+      }
+    }
   }
 
-  res.render("shops/detail.html", { shop, cartLength });
+  res.render("shops/detail.html", { shop, cartLength, sameShops });
 };
