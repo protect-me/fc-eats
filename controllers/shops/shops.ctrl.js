c26fe462c7a098360dc14366eb7468bc84b62e63
protect-me <pmfwiww@gmail.com>
2021-06-27 15:08:27 +0900

    Feat: like sequelize association method

--- controllers/shops/shops.ctrl.js
+++ controllers/shops/shops.ctrl.js
@@ -2,26 +2,46 @@ const models = require("../../models");
 const Shops = require("../../models/Shops");
 
 exports.get_shops_detail = async (req, res) => {
-  const shop = await models.Shops.findOne({
-    where: { id: req.params.id },
-    include: ["Menu"],
-  });
+  try {
+    const shop = await models.Shops.findOne({
+      where: { id: req.params.id },
+      include: ["Menu", "LikeUser"],
+    });
 
-  let cartList = {}; //장바구니 리스트
-  let cartLength = 0;
-  let sameShops = true;
+    let active = false;
+    if (req.isAuthenticated()) {
+      const user = await models.User.findByPk(req.user.id);
+      active = await shop.hasLikeUser(user);
+    }
 
-  if (typeof req.cookies.cartList !== "undefined") {
-    cartList = JSON.parse(unescape(req.cookies.cartList));
-    cartLength = Object.keys(cartList).length;
-    for (let key in cartList) {
-      if (parseInt(cartList[key].shop_id) !== parseInt(req.params.id)) {
-        sameShops = false;
+    const countLike = await shop.countLikeUser();
+
+    let cartList = {}; //장바구니 리스트
+    //쿠키가 있는지 확인해서 뷰로 넘겨준다
+    let cartLength = 0;
+    let sameShops = true;
+    if (typeof req.cookies.cartList !== "undefined") {
+      //장바구니데이터
+      cartList = JSON.parse(unescape(req.cookies.cartList));
+
+      cartLength = Object.keys(cartList).length;
+
+      for (let key in cartList) {
+        if (cartList[key].shop_id !== parseInt(req.params.id))
+          sameShops = false;
       }
     }
-  }
 
-  res.render("shops/detail.html", { shop, cartLength, sameShops });
+    res.render("shops/detail.html", {
+      shop,
+      cartLength,
+      sameShops,
+      active,
+      countLike,
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 exports.post_shops_like = async (req, res) => {
